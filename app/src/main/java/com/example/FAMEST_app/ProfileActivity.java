eab51c6687368b30b385c1c42bd6aae898d40c08
Francisco Campos <fcampos@inegi.up.pt>
2021-05-20 10:15:52 +0100

    Memorização de perfis

--- app/src/main/java/com/example/FAMEST_app/ProfileActivity.java
+++ app/src/main/java/com/example/FAMEST_app/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.example.FAMEST_app;
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -10,9 +11,18 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.os.Environment;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,6 +31,7 @@ import androidx.annotation.Nullable;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
@@ -30,11 +41,22 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Objects;
+import java.util.Set;
 
 public class  ProfileActivity extends AppCompatActivity {
 
     private static final int PERMISSION_REQUEST_CODE = 1;
     private File file;
+    File[] list_file;
+    ListView profile_view;
+    ArrayAdapter<String> f_adapter;
+    File dir;
+    String f_del;
+    File del;
+    String[] profile_list;
+
+    private Button load;
 
     private EditText mEditTextNome;
     private EditText mEditTextIdade;
@@ -51,12 +73,16 @@ public class  ProfileActivity extends AppCompatActivity {
     public static String Peso;
     public static String Numero_Sapato;
 
+    protected FrameLayout frameLayout;
+    protected ConstraintLayout layout;
+
     public static Boolean perfil = false;
 
     //Apresentação da página onde o utilizador irá preencher algumas informações úteis como nome, altura, etc
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+        layout = (ConstraintLayout) findViewById(R.id.xmlprofile);
 
         checkHomem = (CheckBox) findViewById(R.id.checkBoxHomem);
         checkMulher = (CheckBox) findViewById(R.id.checkBoxMulher);
@@ -65,6 +91,35 @@ public class  ProfileActivity extends AppCompatActivity {
         mEditTextAltura = (EditText) findViewById(R.id.editTextAltura);
         mEditTextPeso = (EditText) findViewById(R.id.editTextPeso);
         mEditTextNumeroSapato = (EditText) findViewById(R.id.editTextNumeroSapato);
+
+        load = (Button) findViewById(R.id.load);
+
+        frameLayout = (FrameLayout) findViewById(R.id.listprofile);
+        getLayoutInflater().inflate(R.layout.profile_list,frameLayout);
+
+        profile_view = (ListView) findViewById(R.id.profiles);
+        registerForContextMenu(profile_view);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        f_del=f_adapter.getItem(info.position);
+        del = new File(dir, f_del);
+        if (item.getItemId() == R.id.delete) {
+            //Log.d("ContM", del.getName());
+            del.delete();
+            load.performClick();
+            Toast.makeText(this, "Perfil removido", Toast.LENGTH_SHORT).show();
+
+        }
+        return super.onContextItemSelected(item);
     }
 
     //Com esta função pretende-se seleccionar apenas uma das opções relativamente ao gênero do utilizador
@@ -151,13 +206,15 @@ public class  ProfileActivity extends AppCompatActivity {
         if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
             Toast.makeText(this, "Write External Storage permission allows us to do store images. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
         } else {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE,android.Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
         }
     }
 
     //Função que permite aferir se o utilizador concedeu permissão para aceder à memória interna do smartphone
     private boolean checkPermission() {
-        int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) +
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE);
         if (result == PackageManager.PERMISSION_GRANTED) {
             return true;
         } else {
@@ -178,26 +235,6 @@ public class  ProfileActivity extends AppCompatActivity {
                 break;
         }
     }
-
-
-    /*private void requestPermission_read() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            Toast.makeText(this, "Write External Storage permission allows us to do store images. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
-        }
-    }
-
-    //Função que permite aferir se o utilizador concedeu permissão para aceder à memória interna do smartphone
-    private boolean checkPermission_read() {
-        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
-        if (result == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        } else {
-            return false;
-        }
-    }*/
-
 
 
     private void gravar_dados() {
@@ -266,7 +303,7 @@ public class  ProfileActivity extends AppCompatActivity {
         try {
             FileOutputStream fos = new FileOutputStream(file);
 
-            fos.write((Nome+ System.getProperty("line.separator") + gender + System.getProperty("line.separator") + Altura +  System.getProperty("line.separator") + Peso +
+            fos.write((Nome+ System.getProperty("line.separator") + Idade + System.getProperty("line.separator") + gender + System.getProperty("line.separator") + Altura +  System.getProperty("line.separator") + Peso +
                     System.getProperty("line.separator") + Numero_Sapato).getBytes());
 
             fos.close();
@@ -283,8 +320,13 @@ public class  ProfileActivity extends AppCompatActivity {
     }
 
     private void listprofile() {
+
+        frameLayout.setVisibility(View.VISIBLE);
+        layout.setVisibility(View.INVISIBLE);
+
+
         String root = Environment.getExternalStorageDirectory().toString();
-        File dir = new File(root + "/FAMEST"+"/Perfis");
+        dir = new File(root + "/FAMEST"+"/Perfis");
 
         //Criação de um diretório para onde será enviado o ficheiro txt
         if (!dir.exists()) {
@@ -292,9 +334,29 @@ public class  ProfileActivity extends AppCompatActivity {
             dir.mkdir();
 
         }
+        list_file = dir.listFiles();
+        //File [] list_file = dir.listFiles();
+        profile_list = new String[list_file.length];
+        int count = 0;
+        for (File f : list_file) {
+            String name = f.getName();
+            profile_list[count]= name;
+            count++;
+        }
+        f_adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, profile_list);
+
+        profile_view.setAdapter(f_adapter);
+        profile_view.setOnItemClickListener(myclickLis);
 
 
-        File read_file = new File(dir, "Francisco.txt");
+    }
+
+    private final AdapterView.OnItemClickListener myclickLis = (parent, view, position, id) -> {
+        frameLayout.setVisibility(View.INVISIBLE);
+        layout.setVisibility(View.VISIBLE);
+
+        String title = ((TextView) view).getText().toString();
+        File read_file = new File(dir, title);
 
 
         //Read text from file
@@ -314,5 +376,28 @@ public class  ProfileActivity extends AppCompatActivity {
             //You'll need to add proper error handling here
         }
         Log.d("Res", text.toString());
+        String [] array_dados = text.toString().split("\n");
+        mEditTextNome.setText(array_dados[0]);
+        mEditTextIdade.setText(array_dados[1]);
+        mEditTextAltura.setText(array_dados[3]);
+        mEditTextPeso.setText(array_dados[4]);
+        mEditTextNumeroSapato.setText(array_dados[5]);
+
+        if (array_dados[2].equals("Masculino")){
+            checkMulher.setChecked(false);
+            checkHomem.setChecked(true);
+        } else {
+            checkHomem.setChecked(false);
+            checkMulher.setChecked(true);
+        }
+
+
+    };
+
+    private String[] remove_arr (String[] arr, int pos) {
+        for (int i = pos; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        return arr;
     }
 }
